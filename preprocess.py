bcd0edd50e2dd95312d51e1430e9ae80e133466b
Ajay Kote <akajaykote@gmail.com>
2026-02-18 19:56:57 +0530

    week 2 Milestone

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,13 @@
 import cv2
 import numpy as np
 
-IMG_SIZE = 256  # change only if needed
-
+IMG_SIZE = 256  
 
 def preprocess_image(image_path):
     image = cv2.imread(image_path)
     image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
     image = image / 255.0
     return image
-
 
 def preprocess_mask(mask_path):
     mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
