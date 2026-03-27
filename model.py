f6bf1d86fac0d42331f0b3d0fba4d67924246716
Ajay Kote <akajaykote@gmail.com>
2026-03-27 22:05:05 +0530

    milestopne 3 done

--- model.py
+++ model.py
@@ -1,49 +1,27 @@
 import torch
 import torch.nn as nn
+from torchvision.models.segmentation import deeplabv3_resnet50
 
-class DoubleConv(nn.Module):
-    def __init__(self, in_ch, out_ch):
+
+class DeepLabV3(nn.Module):
+
+    def __init__(self, pretrained_backbone: bool = True):
         super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            nn.ReLU(inplace=True),
+
+        model = deeplabv3_resnet50(
+            weights="DEFAULT" if pretrained_backbone else None
         )
 
-    def forward(self, x):
-        return self.conv(x)
+        # Replace classifier to output 1 channel (binary segmentation)
+        in_ch = model.classifier[-1].in_channels
+        model.classifier[-1] = nn.Conv2d(in_ch, 1, kernel_size=1)
 
-class UNet(nn.Module):
-    def __init__(self):
-        super().__init__()
+        self.model = model
 
-        self.down1 = DoubleConv(3, 64)
-        self.pool1 = nn.MaxPool2d(2)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.model(x)
+        return out["out"]
 
-        self.down2 = DoubleConv(64, 128)
-        self.pool2 = nn.MaxPool2d(2)
 
-        self.middle = DoubleConv(128, 256)
-
-        self.up2 = nn.ConvTranspose2d(256, 128, 2, stride=2)
-        self.conv2 = DoubleConv(256, 128)
-
-        self.up1 = nn.ConvTranspose2d(128, 64, 2, stride=2)
-        self.conv1 = DoubleConv(128, 64)
-
-        self.out = nn.Conv2d(64, 1, 1)
-
-    def forward(self, x):
-        d1 = self.down1(x)
-        d2 = self.down2(self.pool1(d1))
-
-        mid = self.middle(self.pool2(d2))
-
-        u2 = self.up2(mid)
-        c2 = self.conv2(torch.cat([u2, d2], dim=1))
-
-        u1 = self.up1(c2)
-        c1 = self.conv1(torch.cat([u1, d1], dim=1))
-
-        return self.out(c1)
+# Alias for compatibility with existing scripts
+UNet = DeepLabV3
